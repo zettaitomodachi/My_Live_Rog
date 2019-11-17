589435f98b9370ca20f4a2a10ecb99e607d601ed
mitsuki taenaka <mitsuki20040927@gmail.com>
2019-11-17 12:28:32 +0900

    はじめに真ん中表示されるようになった！！

---
+++ My_Live_Rog/TabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarController.swift
+//  My_Live_Rog
+//
+//  Created by 妙中美月 on 2019/11/17.
+//  Copyright © 2019 妙中美月. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        selectedIndex = 1
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
